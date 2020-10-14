a9a26661975f566a8bd6aa0e4b79d8950babd7ad
boycehbz <buzhenhuang@qq.com>
2020-10-14 20:47:19 +0800

    minor changes

--- code/main.py
+++ code/main.py
@@ -80,8 +80,8 @@ def main(**args):
 
 if __name__ == "__main__":
 
-    sys.argv = ["", "--config=cfg_files\\fit_smpl.yaml"
-    ] 
+    # sys.argv = ["", "--config=cfg_files\\fit_smpl.yaml"
+    # ] 
     args = parse_config()
     main(**args)
 
